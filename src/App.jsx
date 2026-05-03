fcbe34bfea64eccc8765b0953a219295a805eb5b
EltonmMustafaj <elton.mustafaj@universitetiaab.com>
2026-05-03 15:39:49 +0200

    com

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
@@ -9,6 +10,7 @@ import ReferencesPage from "./pages/ReferencesPage";
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-white flex flex-col">
         <Navbar />
         <main className="flex-grow">
